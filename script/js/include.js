1f9618b38735ad78a24ff0546a69b59314927036
Do Young <hello03122@naver.com>
2024-04-01 22:10:24 +0900

    sec01

--- script/js/include.js
+++ script/js/include.js
@@ -2,5 +2,6 @@
 $(document).ready( function() {
     $("#header").load("/outline/header.html");
     $("#mainBanner").load("/component/main/main_banner.html");
+    $("#main_sec01").load("/component/main/main_sec01.html");
     $("#footer").load("/outline/footer.html"); 
 });
