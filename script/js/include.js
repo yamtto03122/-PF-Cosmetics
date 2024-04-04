bee3bb9e60286ea7f3d66a3283b15fcb3440a9c1
Do Young <hello03122@naver.com>
2024-04-05 00:19:26 +0900

    main best seller

--- script/js/include.js
+++ script/js/include.js
@@ -3,5 +3,6 @@ $(document).ready( function() {
     $("#header").load("/outline/header.html");
     $("#mainBanner").load("/component/main/main_banner.html");
     $("#main_sec01").load("/component/main/main_sec01.html");
+    $("#main_best").load("/component/main/main_best.html");
     $("#footer").load("/outline/footer.html"); 
 });
